6895e67d8a6663a8d7be43d10e38bcba3587768b
Ihsan <ihsancham7@gmail.com>
2020-08-08 20:34:36 +0200

    last commit from checkout bla bla

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -5,10 +5,10 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("Our first commit from local repo");
 		System.out.println("Our second commit from local repository");
-
 		System.out.println("Now I am on my ihsan branch");
+		System.out.println("I want to have a conflict");
 
-		System.out.println("ihsanbaba branch next commit");
+		System.out.println("I want to have another conflict");
 
 	}
 }
